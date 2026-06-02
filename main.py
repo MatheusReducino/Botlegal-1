42584c2e00d2a3bf39af6ae1466c16a5a8ae422a
Reducino <matheusrecosta@gmail.com>
2026-06-01 22:24:27 -0300

    feat: cria comando pfp, que pega o url da foto de perfil do usuario que chama o comando, e o responde com uma mensagem e a url, que o discord transforma em uma foto visivel

--- main.py
+++ main.py
@@ -19,4 +19,10 @@ async def somalegal(ctx, num1, num2):
     soma = numb1 + numb2
     autor = ctx.author.name
     await ctx.reply(f"oie {autor}, a soma disso ai é {soma}")
+
+@bot.command()
+async def pfp(ctx):
+    pfp = ctx.author.display_avatar.url
+    await ctx.reply(f"a foto do bobao ai hahaha {pfp}")
+
 bot.run("Token do teu bot")
