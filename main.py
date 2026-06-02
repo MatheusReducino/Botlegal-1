d887c28dde7709d805bd1f9e36a5965fd694abc6
Reducino <matheusrecosta@gmail.com>
2026-06-01 22:04:27 -0300

    feat: criando comando ping, que responde o usuario com o ctx.reply, ctx (command context) e basicamente um objeto que tem um monte de informação

--- main.py
+++ main.py
@@ -7,4 +7,8 @@ bot = commands.Bot(command_prefix="$", intents=intents)
 async def on_ready():
     canal = bot.get_channel(id_do_canal)
     await canal.send("Puf, inicializado com sucesso")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.reply(f"pong, haha ganhei")
 bot.run("Token do teu bot")
