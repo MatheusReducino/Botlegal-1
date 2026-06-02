28aa5bd029cde720f7d13876c00ae426b0035bd6
Reducino <matheusrecosta@gmail.com>
2026-06-01 21:56:02 -0300

    feat: criando evento que ao bot ligar, manda uma mensagem em um canal especifico, canal esse obtido pelo id, dentro do proprio discord

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ from discord.ext import commands
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="$", intents=intents)
 
+@bot.event
+async def on_ready():
+    canal = bot.get_channel(id_do_canal)
+    await canal.send("Puf, inicializado com sucesso")
 bot.run("Token do teu bot")
